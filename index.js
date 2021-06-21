c4a6fa19723ccdb06d18962d056c211ca5c3296a
Vignesh-2896 <vigneshb2896@gmail.com>
2021-06-21 16:34:47 +0530

    Final Update.

--- index.js
+++ index.js
@@ -1,58 +1,29 @@
+const Player = (symbol) => {
+    playerSymbol = symbol;
+    playerNumber = gameController.playerList.length + 1;
+
+    return {playerNumber, playerSymbol};
+};
+
 const gameController = (() => {
-  
-    let currentPlayer = 0;
-    let playerList = [];
-    let winner = false;
-    let gameMoves = 0;
-    let gameState = [["","",""],["","",""],["","",""]];
+    playerList = [];
+    gameWinner = false;
 
-    const Player = (playerName, playerSymbol) => {
-        let moveSymbol = playerSymbol;
-        return {playerName, moveSymbol};
-    };
-
-    function startGame(userList){
-        setUpArray();
-        playerList = userList;
-    };
-
-    function setUpArray(){
-        document.querySelectorAll("td").forEach(tdBox => {
-            tdBox.addEventListener("click",function(){
-                addXO(tdBox);
-            });
+    document.querySelectorAll("td").forEach(tdBox => {      // Onclick listener added to all the boxes.
+        tdBox.addEventListener("click",function(){
+            displayController.addToBox(tdBox);
         });
-    };
+    });    
 
-    function addXO(currentBox){
-        currentPlayer = fetchCurrentPlayer();
-        if(currentBox.textContent == "" && winner == false){
-            currentBox.textContent = currentPlayer.moveSymbol;
-            let currentRow = findElementDetails(currentBox,"row");
-            let currentColumn = findElementDetails(currentBox,"column");
-            gameState[currentRow][currentColumn] = currentPlayer.moveSymbol;
-            gameMoves ++;
-            let winnerStatus = false; winnerStatus = checkGameWinner(currentPlayer.moveSymbol, currentRow, currentColumn);
-            if(winnerStatus == true){
-                alert(currentPlayer.playerName+" has won !")
-                winner = true;
-            }
-            if(gameMoves == 9){
-                alert("Tie Game !");
-                return;
-            }            
-        }
-    };
-
-    function fetchCurrentPlayer(){
-        if(gameMoves%2 == 0) return playerList[0];
+    const fetchCurrentPlayer = () => {      // Find which player's turn it is based on number of moves.
+        if(gameBoard.gameMoves%2==0) return playerList[0];
         else return playerList[1];
     };
 
-    function checkGameWinner(symbolToSearch, currentRow, currentColumn){
-        let rowWinner = false; rowWinner = checkRowWinner(symbolToSearch, currentRow);
-        let columnWinner = false; columnWinner = checkColumnWinner(symbolToSearch, currentRow);
-        let diagWinner = false;   diagWinner = checkDiagWinner(symbolToSearch);
+    const checkGameWinner = (symbolToSearch, currentRow, currentColumn) => {       
+        let rowWinner = false; rowWinner = checkRowWinner(symbolToSearch, currentRow);  // Find if game is won based on row where symbol was inserted
+        let columnWinner = false; columnWinner = checkColumnWinner(symbolToSearch, currentColumn);  // Find if game is won based on column where symbol was inserted
+        let diagWinner = false;   diagWinner = checkDiagWinner(symbolToSearch);         // Find if game is won based on diagonals.
         if(rowWinner || columnWinner || diagWinner){
             return true;
         } else {
@@ -60,48 +31,113 @@ const gameController = (() => {
         }
     };
 
-    function checkRowWinner(symbolToSearch, currentRow){
+    const checkRowWinner = (symbolToSearch, currentRow) => {        // Function to check if current row forms a series of same symbols
         symbolPresentCount = 0;
-        for(let i =0;i<=2;i++) if(gameState[currentRow][i] == symbolToSearch) symbolPresentCount++;
+        for(let i =0;i<=2;i++) if(gameBoard.gameState[currentRow][i] == symbolToSearch) symbolPresentCount++;
         if(symbolPresentCount == 3){
-            updateBackground("row",currentRow);
+            displayController.updateBackground("row",currentRow);
             return true;
         }
         else return false;
     }
 
-    function checkColumnWinner(symbolToSearch, currentColumn){
+    const checkColumnWinner = (symbolToSearch, currentColumn) => {  // Function to check if current column forms a series of same symbols
         symbolPresentCount = 0;
-        for(let i =0;i<=2;i++) if(gameState[i][currentColumn] == symbolToSearch) symbolPresentCount++;
+        for(let i =0;i<=2;i++) if(gameBoard.gameState[i][currentColumn] == symbolToSearch) symbolPresentCount++;
         if(symbolPresentCount == 3){
-            updateBackground("column",currentColumn);
+            displayController.updateBackground("column",currentColumn);
             return true;
         }
         else return false;
     }
 
-    function checkDiagWinner(symbolToSearch){
+    const checkDiagWinner = (symbolToSearch) => {
         symbolPresentCount = 0;
-        for(let i=0;i<=2;i++) for(let j=0;j<=2;j++) if(i==j) if(gameState[i][j] == symbolToSearch) symbolPresentCount++;
+// Diagonal being checked is of cells 0-0, 1-1 and 2-2. (Left to Right)
+        for(let i=0;i<=2;i++) for(let j=0;j<=2;j++) if(i==j) if(gameBoard.gameState[i][j] == symbolToSearch) symbolPresentCount++;
         if(symbolPresentCount == 3){
-            updateBackground("diagonal");
+            displayController.updateBackground("diagonal");
             return true;
         }
-        else symbolPresentCount = 0; for(let i=0;i<=2;i++) for(let j=0;j<=2;j++) if(i+j == 2) if(gameState[i][j] == symbolToSearch) symbolPresentCount++;
+// Diagonal being checked is of cells 0-2, 1-1 and 2-0. (Right to Lefts)      
+        else symbolPresentCount = 0; for(let i=0;i<=2;i++) for(let j=0;j<=2;j++) if(i+j == 2) if(gameBoard.gameState[i][j] == symbolToSearch) symbolPresentCount++;
         if(symbolPresentCount == 3){
-            updateBackground("reverse-diagonal");
+            displayController.updateBackground("reverse-diagonal");
             return true;
         }
         else return false;
     }
 
-    function findElementDetails(currentBox, lineType){
-        boxPosition = currentBox.getAttribute("data-game-pos");
-        if(lineType == "row") return parseInt(boxPosition.slice(0,1));
-        if(lineType == "column") return parseInt(boxPosition.slice(2));
+    return {
+        playerList,
+        gameWinner,
+        checkGameWinner,
+        fetchCurrentPlayer,
+    }
+})();
+
+const gameBoard = (() => {
+
+    let gameMoves = 0;
+    let gameState = [["","",""],["","",""],["","",""]]; // A GameBoard variable to hold the contents of the game i.e. where  which element is present so that winner can be found.
+
+    const Player1 = Player("X");    gameController.playerList.push(Player1);
+    const Player2 = Player("O");    gameController.playerList.push(Player2);
+
+    const findElementPosition = (currentBox, positionType) => {     // Find the matrix position of the current box that was clicked.
+        boxPosition = currentBox.getAttribute("data-matrix-position");
+        if(positionType == "row") return parseInt(boxPosition.slice(0,1));
+        if(positionType == "column") return parseInt(boxPosition.slice(2));
+    };
+
+    const reset = () => {       // Function to reset the elements of the Game.
+        gameBoard.gameState = [["","",""],["","",""],["","",""]];
+        gameBoard.gameMoves = 0;
+        document.querySelectorAll("td").forEach(tdBox => {
+            tdBox.textContent = "";
+            tdBox.style.backgroundColor = "";
+        });
     }
 
-    function updateBackground(elementType, elementIndex = 0){
+    return {
+        gameMoves,
+        gameState,
+        findElementPosition,
+        reset,
+    };
+
+})();
+
+const displayController = (() => {
+
+
+    let resetButton = document.querySelector("#reset_game");
+    resetButton.addEventListener("click", gameBoard.reset);
+
+    const addToBox = (currentBox) => {          // Function called when a tic-tac-toe box is clicked on.
+        let validMove = false;
+        if(currentBox.textContent == "" && gameController.gameWinner == false) validMove = true;
+        if(validMove){      // Symbol is added if there is no winner yet and if current box is not filled.
+            currentPlayer = gameController.fetchCurrentPlayer();
+            currentPlayerSymbol = currentPlayer.playerSymbol;
+            currentRow = gameBoard.findElementPosition(currentBox, "row");  // Find the matrix row postion using the data attribute attached to the box.
+            currentColumn = gameBoard.findElementPosition(currentBox, "column");    // Find the matrix column postion using the data attribute attached to the box.
+            gameBoard.gameState[currentRow][currentColumn] = currentPlayerSymbol    // GameBoard is updated to hold the symbol at the required position.
+            gameBoard.gameMoves ++;
+            currentBox.textContent = currentPlayerSymbol;
+            let winnerStatus = false; winnerStatus = gameController.checkGameWinner(currentPlayerSymbol, currentRow, currentColumn);
+            if(winnerStatus == true){
+                alert("Player "+currentPlayer.playerNumber+" ("+currentPlayerSymbol+") has Won!");
+                gameController.gameWinner = true;
+            }
+            if(gameBoard.gameMoves == 9 && gameController.gameWinner == false){       // If no winner is found after 9 moves, game is tied.
+                alert("Tie Game !");
+                return;
+            } 
+        }
+    };
+
+    const updateBackground = (elementType, elementIndex = 0) => {       // Function to find and update the cells which form a series.
         if(elementType == "row"){
             tdList = [[elementIndex+"-0"],[elementIndex+"-1"],[elementIndex+"-2"]];
             tdObjectList = findBoxes(tdList);
@@ -116,16 +152,16 @@ const gameController = (() => {
             tdObjectList = findBoxes(tdList);      
         }
         tdObjectList.forEach(function(tdBox){
-            tdBox.style.backgroundColor = "blue";
+            tdBox.style.backgroundColor = "DodgerBlue";
         })
     }
 
 
-    function findBoxes(tdList){
+    const findBoxes = (tdList) => {
         let i = 0; returnList = [] ;
         for(let i =0;i<=2;i++){
             document.querySelectorAll("td").forEach(currentBox => {
-                if(currentBox.getAttribute("data-game-pos") == tdList[i]){
+                if(currentBox.getAttribute("data-matrix-position") == tdList[i]){
                     returnList.push(currentBox);
                     return;
                 }
@@ -134,12 +170,10 @@ const gameController = (() => {
         return returnList;
     }
 
-    return {
-        startGame,
-        Player,
-    };
-})();
 
-player1 = gameController.Player("Jim","X");
-player2 = gameController.Player("Dwight","O");
-gameController.startGame([player1, player2]);
+    return {
+        addToBox,
+        updateBackground,
+    }
+
+})();
